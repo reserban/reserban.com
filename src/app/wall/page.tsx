375388b4efe61878d679ba06eebf2c0db9d85f5a
Alexandru (The) Serban <alexreserban@gmail.com>
2025-03-18 15:10:30 +0200

    fix

--- src/app/wall/page.tsx
+++ src/app/wall/page.tsx
@@ -1,10 +1,5 @@
-import Hero from "@/components/pages/hero";
 import Navbar from "@/components/pages/navbar";
-import FAQ from "@/components/pages/faq";
 import Footer from "@/components/pages/footer";
-import Process from "@/components/pages/process";
-import Testimonial from "@/components/pages/testimonial";
-import Values from "@/components/pages/values";
 import Work from "@/components/pages/work";
 
 export default function Home() {
